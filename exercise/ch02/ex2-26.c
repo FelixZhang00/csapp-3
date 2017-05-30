085bb56835d91e044e7b70bb43a606e38aaa851d
root <root@localhost.localdomain>
2017-05-29 23:57:59 -0400

    ex-2-26

--- exercise/ch02/ex2-26.c
+++ exercise/ch02/ex2-26.c
@@ -5,6 +5,10 @@
 /*Determine weather string s is longer than string t*/
 /*WARING:This function is buggy*/
 int strlonger(char *s, char *t){
+  /* 当strlen(s) 比 strlen(t)小时，得到负数，
+   * 因为与0比较大小， 
+   * 然后又被转换为一个非常大的无符号数
+   */	
   return strlen(s) - strlen(t) > 0;
 }
 
