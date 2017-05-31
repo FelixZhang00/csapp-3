def3b8519f1410070ffe3ad101482c3fe1a3bc2b
Felix <l836828946@gmail.com>
2017-05-31 01:41:59 -0400

    sample-ch03

---
+++ sample/ch03/mstore.s
@@ -0,0 +1,22 @@
+	.file	"mstore.c"
+	.text
+	.p2align 4,,15
+	.globl	multstore
+	.type	multstore, @function
+multstore:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movq	%rdx, %rbx
+	call	mult2@PLT
+	movq	%rax, (%rbx)
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	multstore, .-multstore
+	.ident	"GCC: (Debian 6.3.0-12) 6.3.0 20170406"
+	.section	.note.GNU-stack,"",@progbits
