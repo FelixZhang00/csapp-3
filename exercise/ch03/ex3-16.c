17b96431828036a09994d19d2be2d5071c26946c
Felix <l836828946@gmail.com>
2017-06-01 21:04:31 -0400

    ex3-16 answer2

--- exercise/ch03/ex3-16.c
+++ exercise/ch03/ex3-16.c
@@ -1,10 +1,10 @@
 
 void cond(long a,long *p){
-  if(p && a> *p){
-    goto LL;
-  }
-  return;
-LL:
-  *p=a;
+  if(p==0)
+    goto done;
+  if(a <= *p)
+    goto done;
+  *p = a;
+done:
   return;
 }
